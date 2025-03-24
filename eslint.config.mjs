6769d0c9ca3b2a4a03d70297d4785e720b70d521
Hashim Karim <hashimkarim168@gmail.com>
2025-03-24 17:52:52 +0100

    Less strict eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,25 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+	{
+		files: [
+			"app/page.tsx",
+			"components/navbar.tsx",
+			"components/highlight-section.tsx",
+			"components/call-to-action.tsx",
+			"components/sponsorship-oppurtunities.tsx",
+			"components/hero-section.tsx",
+		],
+		rules: {
+			"@typescript-eslint/no-unused-vars": "off", // Disable for unused variables
+			"react/no-unescaped-entities": "off", // Disable for unescaped apostrophes
+		},
+	},
 ];
 
 export default eslintConfig;
