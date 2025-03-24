4a34645f8248226ea4b781d2ffe0d2eb30060cbb
Hashim Karim <hashimkarim168@gmail.com>
2025-03-24 19:30:45 +0100

    Eslint ignore now applies for all ts files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,14 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
 	...compat.extends("next/core-web-vitals", "next/typescript"),
 	{
-		files: [
-			"app/page.tsx",
-			"components/navbar.tsx",
-			"components/highlight-section.tsx",
-			"components/call-to-action.tsx",
-			"components/sponsorship-oppurtunities.tsx",
-			"components/hero-section.tsx",
-		],
+		files: ["**/*.ts", "**/*.tsx"],
 		rules: {
 			"@typescript-eslint/no-unused-vars": "off", // Disable for unused variables
 			"react/no-unescaped-entities": "off", // Disable for unescaped apostrophes
